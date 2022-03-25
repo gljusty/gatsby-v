85b27cd0618bb6ef76d93cd8dfe378cf915c001b
gljusty <stapleton_bryan@yahoo.com>
2022-03-25 13:59:07 -0600

    pushing wip to test dns; should be setup at gljusty.dev

---
+++ src/components/mycomponent.js
@@ -0,0 +1,12 @@
+import * as react from "react";
+import styled from "styled-components";
+
+const mycomponent = () => {
+    const button = styled.button`
+    height: 20px;
+    width: 80px;
+    background-color: black;
+    `
+}
+
+export default mycomponent
