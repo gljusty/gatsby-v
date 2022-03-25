f9461589dd7e0b0bbd38a97aa2af8c8faf3d459f
gljusty <stapleton_bryan@yahoo.com>
2022-03-24 22:11:35 -0600

    changing gatsby config testing build

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,9 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
-    siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
+    title: `Bryan's Portfolio`,
+    description: `A static site built with gatsby to show off some of my work.`,
+    author: `@gljusty`,
+    siteUrl: `https://github.com/gljusty`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
