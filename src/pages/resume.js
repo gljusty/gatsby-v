bcc1cd972755e761e46a328f01ce33ba7b6ce756
gljusty <stapleton_bryan@yahoo.com>
2022-03-28 18:36:00 -0600

    remove resumedisplay temporarily

--- src/pages/resume.js
+++ src/pages/resume.js
@@ -1,12 +1,10 @@
 import * as React from "react"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
-import ResumeDisplay from "../components/resumedisplay"
 
 const ResumePage = () => (
     <Layout>
-        <Seo title="Resume" />  
-        <ResumeDisplay></ResumeDisplay>
+        <Seo title="Resume" /> 
     </Layout>
 )
 
