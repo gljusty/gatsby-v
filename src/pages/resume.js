9ff4f0aee7d64fc39054dbebf7b5c4da7be4862a
gljusty <stapleton_bryan@yahoo.com>
2022-03-28 18:32:44 -0600

    move pdfjs

--- src/pages/resume.js
+++ src/pages/resume.js
@@ -1,24 +1,15 @@
 import * as React from "react"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
-import { pdfjs } from 'react-pdf';
 import ResumeDisplay from "../components/resumedisplay"
-
-pdfjs.GlobalWorkerOptions.workerSrc = 'pdf.worker.min.js';
 
 const ResumePage = () => (
     <Layout>
         <Seo title="Resume" />  
-        <ResumeComponent></ResumeComponent>
+        <ResumeDisplay></ResumeDisplay>
     </Layout>
 )
 
-const ResumeComponent = () => {
-    return (
-        <div style={{position: `absolute`, margin: `auto auto`, top: `0`, left: `50%`}}>
-            <ResumeDisplay />
-        </div>
-    )
-}
+
 
 export default ResumePage
