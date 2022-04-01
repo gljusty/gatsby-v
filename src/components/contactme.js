7337891e1f53af422a85d365a760955ad7680fbf
gljusty <stapleton_bryan@yahoo.com>
2022-03-31 18:18:56 -0600

    changing house animation speed + fromatting + adding contact component
    saving restore point

---
+++ src/components/contactme.js
@@ -0,0 +1,13 @@
+import * as React from "react"
+import styled from "styled-components"
+
+const StyledContactMe = styled.div`
+  position: absolute;
+  color: whitesmoke;
+`
+
+const ContactMeComponent = () => {
+  return <StyledContactMe>test</StyledContactMe>
+}
+
+export default ContactMeComponent
