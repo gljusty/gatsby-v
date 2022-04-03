73ea2c5f6db52de7a9c16eca53d7c58de75faf43
gljusty <stapleton_bryan@yahoo.com>
2022-04-03 15:53:35 -0600

    contactme style fix

--- src/components/contactme.js
+++ src/components/contactme.js
@@ -9,61 +9,29 @@ const StyledContactMe = styled.div`
   cursor: pointer;
   display: flex;
   position: fixed;
-  top: 95vh;
-  left: 5vw;
+  top: 97.5vh;
+  left: 0vw;
   background: transparent;
-  padding: 2vh 2vw;
   border-radius: 8px;
   margin: auto auto;
   color: whitesmoke;
 `
 const StyledContactLink = styled.a`
-  margin: 0 1vw 0 1vw;
+  margin: 0 0.5vw 0 0.5vw;
   opacity: 100%;
   font-family: "Courier";
   font-weight: bold;
   text-decoration: none;
   color: aquamarine;
   transition: all 250ms linear;
-  ${StyledContactMe}:hover & {
+  &:hover {
     color: whitesmoke;
   }
 `
 
-const testf = () => {
-  const start = document.querySelector("._contact")
-  const end = document.querySelector("._email_link")
-  if (typeof window !== undefined) {
-    setTimeout(() => {
-      try {
-        const line = new LeaderLine(start, end, {
-          hide: true,
-          color: `aquamarine`,
-          startSocket: `top`,
-          endSocket: `bottom`,
-          path: `grid`,
-          startPlug: `behind`,
-          endPlug: `arrow2`,
-          startSocketGravity: 0,
-          endSocketGravity: 40,
-        })
-        const eline = document.querySelector(".leader-line:last-of-type")
-        eline.style.zIndex = -1
-        line.show("draw")
-      } catch {
-        //do nothing
-      }
-    }, 100)
-  }
-}
-
 const ContactMeComponent = () => {
   return (
-    <StyledContactMe
-      className="_contact animate__animated animate__fadeInUp"
-      onClick={testf}
-      cursor="pointer"
-    >
+    <StyledContactMe className="_contact animate__animated animate__fadeInUp">
       <StyledContactLink
         className="_contact"
         href="mailto:b@gljusty.dev"
